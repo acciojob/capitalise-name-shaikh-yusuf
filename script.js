76f12f65bf6a31c9f1a5b61b91d45ae4656a674f
shaikh-yusuf <94888066+shaikh-yusuf@users.noreply.github.com>
2023-11-18 10:33:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const fnameInput = document.getElementById('fname');
+
+            fnameInput.addEventListener('blur', function () {
+                // Convert the content to uppercase
+                fnameInput.value = fnameInput.value.toUpperCase();
+            });
